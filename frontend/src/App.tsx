d9c68a32d973c8a53c36b349aea8c6aa12a471f3
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-02-05 18:43:39 +0545

    create HomeScreen component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
+      <HomeScreen />
       <Footer />
     </>
   );
---
+++ frontend/src/products.ts
@@ -0,0 +1,79 @@
+const products = [
+  {
+    id: 1,
+    name: "Earthen Bottle",
+    href: "#",
+    price: "$48",
+    description:
+      "Get the full lineup of our Basic AI Bottel. Have a fresh hard drink, and an extra for holiday.",
+    options: "12 colors",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  },
+  {
+    id: 2,
+    name: "Nomad Tumbler",
+    href: "#",
+    price: "$35",
+    description:
+      "Get the full lineup of our Basic Tumbler. Have a fresh AI shit water all week, and an extra for laundry day.",
+    options: "10 colors",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 3,
+    name: "Focus Paper Refill",
+    href: "#",
+    price: "$89",
+    description: "Japenese paper made with hands. True to it's root",
+    options: "3 colors",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt:
+      "Person using a pen to cross a task off a productivity paper card.",
+  },
+  {
+    id: 4,
+    name: "Machined Mechanical Pencil",
+    href: "#",
+    price: "$35",
+    description: "Get the mechanical pencil. Japanese Strength",
+    options: "6 colors",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt:
+      "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  {
+    id: 5,
+    name: "Basic Tee 8-Pack",
+    href: "#",
+    price: "$256",
+    description:
+      "Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.",
+    options: "8 colors",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+    imageAlt:
+      "Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.",
+  },
+  {
+    id: 6,
+    name: "Basic Tee",
+    href: "#",
+    price: "$32",
+    description:
+      "Look like a visionary CEO and wear the same black t-shirt every day.",
+    options: "Black",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+    imageAlt: "Front of plain black t-shirt.",
+  },
+];
+
+export default products;
