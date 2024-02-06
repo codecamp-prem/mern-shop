fac1ac2071bad16a4af3b5df6330434b871a35d0
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-02-06 17:52:26 +0545

    create basic workable product details page

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,4 +1,5 @@
 import { Link, useParams } from "react-router-dom";
+import Rating from "../components/Rating";
 import products from "../products";
 
 const ProductScreen = () => {
@@ -6,40 +7,46 @@ const ProductScreen = () => {
 
   const product = products.find((p) => p.product_id === productId);
 
+  if (!product) {
+    return null;
+  }
   return (
-    <div className="m-4">
-      <div>
-        <Link to="/" className="hover:cursor-pointer">
-          <span className="hover:bg-cyan underline">Go Back</span>
-        </Link>
-      </div>
-      <div className="flex flex-col md:flex-row m-6 space-y-4 md:space-y-0 md:space-x-4 h-1/2">
-        <div className="md:w-1/2">
-          <img
-            src={product?.imageSrc}
-            alt={product?.imageSrc}
-            className="object-cover object-center"
-          />
+    <>
+      <div className="flex flex-col relative container mx-auto p-6">
+        <div className="">
+          <Link to="/" className="hover:cursor-pointer">
+            <span className="hover:bg-cyan underline">Go Back</span>
+          </Link>
         </div>
-        <div className="md:w-1/2">
-          <div className="flex flex-col">
-            <div>{product?.name}</div>
-            <div>{product?.rating}</div>
-            <div>{product?.price}</div>
-            <div>{product?.description}</div>
+        <div className="flex flex-col md:flex-row md:space-x-2 lg:space-x-4 m-2 space-y-3 md:space-y-0">
+          <div className="w-full md:w-[40%]">
+            <img
+              src={product?.imageSrc}
+              alt={product?.imageSrc}
+              className="w-full rounded-xl m-auto pointer-events-none transition duration-300 hover:cursor-pointer hover:shadow-xl"
+            />
           </div>
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <div>Price: {product?.price}</div>
+          <div className="w-full md:w-[60%] items-start">
+            <div>{product?.name}</div>
+            <div className="flex">
+              <Rating rating={product?.rating} />
+            </div>
+            <div>({product?.numReviews} Reviews)</div>
+            <div>{product?.description}</div>
+
+            <div className="">
+              <div>Price: ${product?.price}</div>
               <div>Status: Out of stock</div>
             </div>
             <div>
-              <button>Add to cart</button>
+              <button className="py-2 px-4 rounded-md border bg-gray-200 hover:bg-cyan">
+                Add to cart
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
