d6c605fef34e85cc54fd523abb55a61077ed2bef
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-02-07 12:03:42 +0545

    fix CORS issue by adding proxy and cors key value in vite.config.ts

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,6 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import SingleProduct from "../components/SingleProduct";
-import products from "../products";
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (!products) return null;
+
   return (
     <>
       <div className="bg-white">
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
