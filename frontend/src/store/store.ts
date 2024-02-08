aeba6ee83e363c3b71fdcbc5909f49deaa889874
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-02-08 10:09:51 +0545

    Redux toolKit Query: create product apiSlice and get product endpoints

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,11 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
+
+export type RootState = ReturnType<typeof store.getState>; // return type of function type: RootState
+export type AppDispatch = typeof store.dispatch;
