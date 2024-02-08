9516a6dbca477e72f7deb99c6fc46e52bb9c33c2
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-02-08 14:35:38 +0545

    create carSlice actions

--- frontend/src/store/slices/cartSlice.ts
+++ frontend/src/store/slices/cartSlice.ts
@@ -4,10 +4,45 @@ const initialState = localStorage.getItem("mern-cart")
   ? JSON.parse(localStorage.getItem("mern-cart"))
   : { cartItems: [] };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.id === item.id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x.id === existItem.id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //calculate item price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      // shipping price
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      //tax price(suppose 15%)
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+      //total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("mern-cart", JSON.stringify(state));
+    },
+  },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
