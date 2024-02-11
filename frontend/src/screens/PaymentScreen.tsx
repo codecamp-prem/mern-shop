295210e9398f8d2be86e5cfa69498eb5118651c6
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-02-11 15:00:22 +0545

    fix minor css issue in payment screen

--- frontend/src/screens/PaymentScreen.tsx
+++ frontend/src/screens/PaymentScreen.tsx
@@ -77,115 +77,114 @@ const PaymentScreen = () => {
               Payment
             </h2>
             <fieldset className="mt-4">
-              <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+              <div className="space-y-4 sm:items-center sm:space-x-10 sm:space-y-0">
                 <label
                   htmlFor="card-number"
                   className="block text-sm font-medium text-gray-700"
                 >
                   Payment type
                 </label>
-                {paymentMethods.map((paymentMethod) => (
-                  <div key={paymentMethod.id} className="flex items-center">
-                    <input
-                      id={paymentMethod.id}
-                      name="payment-type"
-                      type="radio"
-                      defaultChecked={
-                        paymentMethod.id === selectedPaymentMethod
-                      }
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      onClick={() =>
-                        handleSelectPaymentMethod(paymentMethod.id)
-                      }
-                    />
-                    <label
-                      htmlFor={paymentMethod.id}
-                      className="ml-3 block text-sm font-medium text-gray-700"
-                    >
-                      {paymentMethod.title}
-                    </label>
-                  </div>
-                ))}
+                <div className="flex space-x-4 m-0">
+                  {paymentMethods.map((paymentMethod) => (
+                    <div key={paymentMethod.id} className="flex items-center">
+                      <input
+                        id={paymentMethod.id}
+                        name="payment-type"
+                        type="radio"
+                        defaultChecked={
+                          paymentMethod.id === selectedPaymentMethod
+                        }
+                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        onClick={() =>
+                          handleSelectPaymentMethod(paymentMethod.id)
+                        }
+                      />
+                      <label
+                        htmlFor={paymentMethod.id}
+                        className="ml-3 block text-sm font-medium text-gray-700"
+                      >
+                        {paymentMethod.title}
+                      </label>
+                    </div>
+                  ))}
+                </div>
               </div>
             </fieldset>
+            {selectedPaymentMethod !== "PayPal" && (
+              <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
+                <div className="col-span-4">
+                  <label
+                    htmlFor="card-number"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Card number
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="card-number"
+                      name="card-number"
+                      autoComplete="cc-number"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
 
-            <div
-              className={`mt-6 grid grid-cols-4 gap-x-4 gap-y-6 ${
-                selectedPaymentMethod !== "PayPal" ? "visible" : "invisible"
-              }`}
-            >
-              <div className="col-span-4">
-                <label
-                  htmlFor="card-number"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Card number
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    id="card-number"
-                    name="card-number"
-                    autoComplete="cc-number"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                <div className="col-span-4">
+                  <label
+                    htmlFor="name-on-card"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Name on card
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="name-on-card"
+                      name="name-on-card"
+                      autoComplete="cc-name"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-span-3">
+                  <label
+                    htmlFor="expiration-date"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Expiration date (MM/YY)
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="expiration-date"
+                      id="expiration-date"
+                      autoComplete="cc-exp"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="cvc"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    CVC
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="cvc"
+                      id="cvc"
+                      autoComplete="csc"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
                 </div>
               </div>
-
-              <div className="col-span-4">
-                <label
-                  htmlFor="name-on-card"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Name on card
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    id="name-on-card"
-                    name="name-on-card"
-                    autoComplete="cc-name"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-3">
-                <label
-                  htmlFor="expiration-date"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Expiration date (MM/YY)
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="expiration-date"
-                    id="expiration-date"
-                    autoComplete="cc-exp"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="cvc"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  CVC
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="cvc"
-                    id="cvc"
-                    autoComplete="csc"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-            </div>
+            )}
           </div>
           {/* Order summary */}
           <div className="mt-10 lg:mt-0">
@@ -243,7 +242,7 @@ const PaymentScreen = () => {
 
                       <div className="flex flex-1 items-end justify-between pt-2">
                         <p className="mt-1 text-sm font-medium text-gray-900">
-                          {product.price}
+                          ${product.price}
                         </p>
 
                         <div className="ml-4">
